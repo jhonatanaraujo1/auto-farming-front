948872b035d03994d13bcb5964f11a50d499b4ac
jhonatanaraujo1 <jhonatan.araujo1@Hotmail.com>
2022-09-29 22:33:32 +0100

    formated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./components/login/login.component";
-import {HomeComponent} from "./components/home/home.component";
 import {ProfileComponent} from "./components/profile/profile.component";
 import {ResetPasswordComponent} from "./components/login/reset-password/reset-password.component";
 
