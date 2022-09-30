20957e5b5ee9c62aea0b8e7446f8a82c92ddc775
jhonatanaraujo1 <jhonatan.araujo1@Hotmail.com>
2022-09-30 13:43:50 +0100

    Creating Profile list layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {LoginComponent} from "./components/login/login.component";
 import {ProfileComponent} from "./components/profile/profile.component";
 import {ResetPasswordComponent} from "./components/login/reset-password/reset-password.component";
 import {SignupComponent} from "./components/login/sign-up/signup.component";
+import {ListProfileComponent} from "./components/profile/list-profile/list-profile.component";
 
 
 const routes: Routes = [
@@ -33,7 +34,11 @@ const routes: Routes = [
     component: ProfileComponent,
 
   },
+  {
+    path: 'listprofile',
+    component: ListProfileComponent,
 
+  },
   { path: '**', redirectTo: 'login' }
 ];
 @NgModule({
