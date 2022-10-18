de250f28675c42c84fe1d92bd09bab62185bc323
jhonatanaraujo1 <jhonatan.araujo1@Hotmail.com>
2022-10-18 19:02:35 +0100

    adjust dynamic tasks

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
   step = 0;
   constructor() { }
-
+  resultado = [
+    [{"task_create_talk": "Converse por 4 horas"}],
+    [{"task_roll_feed": "Rolar o feed por 700 horas sem parar"}],
+    [{"task_like_picutres": "Curta fotos em um espaco de 1 minuto cada foto"}],
+    [{"task_comment_picture": "Comente fotos que faca sentido com seus anuncios"}],
+    [{"task_watch_movies": "Assista videos"}],
+    [{"task_watch lives": "Assista lives por 900 horas sem parar"}],
+  ]
   ngOnInit(): void {
 
   }
